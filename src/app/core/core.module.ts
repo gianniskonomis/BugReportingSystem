7ebf462d96deea3a766778e4b0444d33f28ed4cc
Marios Skazikis <mskazikis@gmail.com>
2018-05-12 13:58:46 +0300

    O

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ],
+  declarations: [],
+  exports: []
+})
+export class CoreModule {
+
+}
