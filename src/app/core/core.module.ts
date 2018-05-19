62385d4c438a9940951fc1e4fc4c463ede0514d6
Lucas-Nt <lntaopoulos@gmail.com>
2018-05-19 17:57:37 +0300

    styles

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { AppheaderComponent } from "./appheader/appheader.component";
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  declarations: [],
-  exports: []
+  declarations: [AppheaderComponent],
+  exports: [
+    AppheaderComponent
+  ]
 })
 export class CoreModule {
 
