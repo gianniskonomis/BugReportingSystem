82310972c031e2895af25c24266a8093cfbe5b3e
Marios Skazikis <mskazikis@gmail.com>
2018-05-19 16:49:02 +0300

    toastr

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,20 @@
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ToastrModule } from "ngx-toastr";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: "toast-bottom-right",
+      preventDuplicates: true
+    })
   ],
   declarations: [],
-  exports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
-  ]
+  exports: [CommonModule, FormsModule, ReactiveFormsModule]
 })
-export class SharedModule { }
+export class SharedModule {}
