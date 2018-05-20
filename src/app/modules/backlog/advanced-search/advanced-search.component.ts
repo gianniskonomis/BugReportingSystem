eeb51a2a06699890075d0cbfbf5d06f3bf50642f
Lucas-Nt <lntaopoulos@gmail.com>
2018-05-20 15:32:24 +0300

    fix

--- src/app/modules/backlog/advanced-search/advanced-search.component.ts
+++ src/app/modules/backlog/advanced-search/advanced-search.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, Output } from '@angular/core';
-import { EventEmitter } from 'protractor';
-
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 @Component({
-  selector: 'app-advanced-search',
-  templateUrl: './advanced-search.component.html',
-  styleUrls: ['./advanced-search.component.css']
+  selector: "app-advanced-search",
+  templateUrl: "./advanced-search.component.html",
+  styleUrls: ["./advanced-search.component.css"]
 })
 export class AdvancedSearchComponent implements OnInit {
 
